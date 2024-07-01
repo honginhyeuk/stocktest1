b90a5573f7ddad947a7069f8a1ecaa37dacfc002
test <inheuk1117@naver.com>
2024-07-02 01:30:55 +0900

    Simplify requirements.txt to resolve dependency conflicts

--- stockapp/models.py
+++ stockapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# stockapp/models.py
+
+from django.db import models
+
+class StockData(models.Model):
+    ticker = models.CharField(max_length=10)
+    date = models.DateField()
+    close = models.FloatField()
+    rsi = models.FloatField()
+    bollinger_high = models.FloatField()
+    bollinger_low = models.FloatField()
+
+    def __str__(self):
+        return f"{self.ticker} on {self.date}"
